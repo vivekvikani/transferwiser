683c9e5f53e53ed004a18a7eb83789e3fc3378d9
Vikani, Vivek | Viki | PAYSD <vivek.vikani@rakuten.com>
2020-03-18 17:30:49 +0900

    Initial Commit

---
+++ src/main/java/com/madeovercode/transferwise/TransferwiseApplication.java
@@ -0,0 +1,13 @@
+package com.madeovercode.transferwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madeovercode/transferwise/TransferwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madeovercode.transferwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
